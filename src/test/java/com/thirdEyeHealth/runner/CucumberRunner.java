7e0ba726bfaa512114dfcd047292b5bea8fbb7f9
KalbinurK <kkalbinur@yahoo.com>
2021-04-16 13:18:17 -0400

    new branch

--- src/test/java/com/thirdEyeHealth/runner/CucumberRunner.java
+++ src/test/java/com/thirdEyeHealth/runner/CucumberRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         glue = "com/thirdEyeHealth/step_definitions",
         dryRun =false,
-        tags = "@signNotificationNote"
+        tags = "@CreateConsult"
         //publish = true
 
 )
